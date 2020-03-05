9da8b17a4a6f6b8de912d81edd1f5ca23f5d60dd
kneradovsky <kneradovsky@gmail.com>
2020-03-05 17:42:55 +0300

    alisa empty request -> display welcome

--- alisa.js
+++ alisa.js
@@ -1,5 +1,6 @@
 let dialogflow = require('dialogflow')
 let structjson = require('./sctructjson')
+let replies = require('./replies')();
 let projectID = 'openbank-296e5';
 let keyFile = './openbank-296e5-55c3ac91d65b.json'
 let client = new dialogflow.SessionsClient({keyFilename: keyFile,projectId: projectID});
@@ -23,18 +24,20 @@ module.exports = async function(req,res) {
             payload : structjson.jsonToStructProto({'alisa': 'true'}),
         }
     };
-    var responses={res:'unknown'};
-    try {
-        responses = await client.detectIntent(dfreq);
-    } catch(err) {
-        console.log(err);
+    alres.response = {text:replies.welcome(),end_session:false};
+    if(intquery!="") {
+        try {
+            responses = await client.detectIntent(dfreq);
+        } catch(err) {
+            console.log(err);
+        }
+        let resp = responses[0]
+        let respjson = structjson.structProtoToJson(resp.queryResult.webhookPayload)
+        let intent_response='Что?'
+        if(respjson.google.richResponse===undefined)
+            intent_response=resp.queryResult.fulfillmentText;
+        else intent_response = respjson.google.richResponse.items.map(e => e.simpleResponse.textToSpeech).join("\n")
+        alres.response = {text:intent_response,end_session:false};
     }
-    let resp = responses[0]
-    let respjson = structjson.structProtoToJson(resp.queryResult.webhookPayload)
-    let intent_response='Что?'
-    if(respjson.google.richResponse===undefined)
-        intent_response=resp.queryResult.fulfillmentText;
-    else intent_response = respjson.google.richResponse.items.map(e => e.simpleResponse.textToSpeech).join("\n")
-    alres.response = {text:intent_response,end_session:false};
     res.status(200).send(JSON.stringify(alres));
 }
